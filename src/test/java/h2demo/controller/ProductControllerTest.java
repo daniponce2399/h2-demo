b7bccf62308bde590c042e31b1e08de984936c7f
daniponce2399 <47412902+daniponce2399@users.noreply.github.com>
2022-11-11 00:58:46 +0100

    feature/Fix imports

--- src/test/java/h2demo/controller/ProductControllerTest.java
+++ src/test/java/h2demo/controller/ProductControllerTest.java
@@ -5,13 +5,8 @@ import h2demo.model.ProductEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Mono;
-
-import java.security.Timestamp;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ProductControllerTest {
